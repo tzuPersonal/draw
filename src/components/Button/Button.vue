1ad2272fdaa0b231b72ebc943f2eca9e30739699
tzu <tzu.personal@gmail.com>
2021-12-25 01:39:58 +0800

    change button set image

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,21 +1,8 @@
 <template>
   <div
     class="button"
-    :style="{
-      'background-image': `url(${buttonImage})`
-    }"
   />
 </template>
-
-<script setup>
-import { toRefs } from 'vue';
-
-const props = defineProps({
-  buttonImage: String,
-});
-const { buttonImage } = toRefs(props);
-console.log(buttonImage);
-</script>
 
 <style lang="scss" scope>
 .button {
